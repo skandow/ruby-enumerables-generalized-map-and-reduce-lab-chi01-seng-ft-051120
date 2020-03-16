de891f8ee832ec7e3c9fd84f2a57ac6d814515d5
Steven Kandow <kando1sr@gmail.com>
2020-03-16 17:49:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # Your Code Here
+def map(array)
+  result = []
+  array_count = 0 
+  while array_count < array.length do 
+    result.push(yield(array[array_count]))
+    array_count += 1
+  end 
+  result
+end
+
+def reduce(array, starting_point = 0)
+  result = starting_point
+  array_count = 0 
+  while array_count < array.length do 
+    result = yield(array[array_count], result)
+    array_count += 1 
+  end 
+  if result == nil 
+  result = false  
+end
+result 
+end 
