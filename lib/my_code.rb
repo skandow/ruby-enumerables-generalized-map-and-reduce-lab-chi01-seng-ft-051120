7410ab7298beea733209df35a89ce98d09890382
Steven Kandow <kando1sr@gmail.com>
2020-03-16 17:34:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,4 +8,13 @@ def map(array)
   end 
   result
 end
-  
+
+def reduce(array, starting_point = 0)
+  result = 0 
+  array_count = 0 
+  while array_count < array.length do 
+    result += yield(array[array_count])
+    array_count += 1 
+  end 
+  result
+end 
