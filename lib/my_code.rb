80ca48c56b10f28f6ba2fa2c30818a16b96913e9
Steven Kandow <kando1sr@gmail.com>
2020-03-16 17:26:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,11 @@
 # Your Code Here
 def map(array)
-  yield
+  result = []
+  array_count = 0 
+  while array_count < array.length do 
+    yield
+    array_count += 1
+  end 
+  result
 end
-
-map(array) {|block|}
   
